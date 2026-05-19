a16e1e8a1fbf6c8cabdc20d438f9c8d4514fcf0e
sidharthmuralidharan19 <sidharthmuralidharan19@gmail.com>
2026-05-19 16:06:42 +0000

    Add CODESPACE_NAME support for dynamic ALLOWED_HOSTS and API URLs

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
@@ -27,14 +29,22 @@ router.register(r'activities', ActivityViewSet)
 router.register(r'workouts', WorkoutViewSet)
 router.register(r'leaderboard', LeaderboardViewSet)
 
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+CODESPACE_API_HOST = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+
 @api_view(['GET'])
 def api_root(request, format=None):
+    if CODESPACE_API_HOST:
+        base_url = f"https://{CODESPACE_API_HOST}/api/"
+    else:
+        base_url = request.build_absolute_uri('api/')
+
     return Response({
-        'users': request.build_absolute_uri('users/'),
-        'teams': request.build_absolute_uri('teams/'),
-        'activities': request.build_absolute_uri('activities/'),
-        'workouts': request.build_absolute_uri('workouts/'),
-        'leaderboard': request.build_absolute_uri('leaderboard/'),
+        'users': f"{base_url}users/",
+        'teams': f"{base_url}teams/",
+        'activities': f"{base_url}activities/",
+        'workouts': f"{base_url}workouts/",
+        'leaderboard': f"{base_url}leaderboard/",
     })
 
 urlpatterns = [
